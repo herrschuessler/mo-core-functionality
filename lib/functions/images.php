c9ea512dfd646507f43a6e01a9f66fa5acb65571
herrschuessler <schreib@herrschuessler.de>
2019-08-07 12:33:21 +0200

    Fix image height calculations and set default ratio to null

--- lib/functions/images.php
+++ lib/functions/images.php
@@ -21,7 +21,7 @@ trait Images {
 		}
 
 		$defaults = [
-			'ratio'   => 1,
+			'ratio'   => null,
 			'min'     => 300,
 			'max'     => 900,
 			'steps'   => 100,
@@ -33,7 +33,8 @@ trait Images {
 
 		$args = wp_parse_args( $args, $defaults );
 
-		$args['ratio'] = floatval( $args['ratio'] );
+		// Parse ratio to float or null
+		$args['ratio'] = is_numeric( $args['ratio'] ) && $args['ratio'] > 0 ? floatval( $args['ratio'] ) : null;
 
 		$data['image']          = $image;
 		$data['class']          = $args['classes'] ? esc_attr( $args['classes'] ) : '';
@@ -46,9 +47,10 @@ trait Images {
 		$data['sizes_webp']     = [];
 		$data['max'] = [
 			'w' => $args['max'],
-			'h' => round( $args['max'] * $args['ratio'] ),
+			'h' => $args['ratio'] ? round( $args['max'] * $args['ratio'] ) : 0,
 		];
-		$size = $args['min'];
+		$width = $args['min'];
+		$height = 0;
 
 		if ( $args['fit'] == 'cover' ) {
 			$data['class'] .= ' cover-img';
@@ -59,54 +61,57 @@ trait Images {
 		}
 
 		// Add image sizes
-		while ( $size <= $args['max'] && $size <= $image->width ) {
-			array_push( $data['sizes_source'], '{{ image.guid|resize(' . $size . ', ' . round( $size * $args['ratio'] ) . ') }} ' . $size . 'w' );
-			array_push( $data['sizes_webp'], '{{ image.guid|resize(' . $size . ', ' . round( $size * $args['ratio'] ) . ')|towebp(70) }} ' . $size . 'w' );
-			$size = $size + $args['steps'];
+		while ( $width <= $args['max'] && $width <= $image->width ) {
+			$height = $args['ratio'] ? round( $width * $args['ratio'] ) : 0;
+			array_push( $data['sizes_source'], '{{ image.guid|resize(' . $width . ', ' . $height . ') }} ' . $width . 'w' );
+			array_push( $data['sizes_webp'], '{{ image.guid|resize(' . $width . ', ' . $height . ')|towebp(70) }} ' . $width . 'w' );
+			$width = $width + $args['steps'];
 		}
 
 		// If last size was smaller than original image dimensions, add original image
-		if ( ( $size - $args['steps'] ) < $image->width && ( $size - $args['steps'] ) < $args['max'] ) {
-			array_push( $data['sizes_source'], '{{ image.guid|resize(' . $image->width . ', ' . round( $image->width * $args['ratio'] ) . ') }} ' . $image->width . 'w' );
-			array_push( $data['sizes_webp'], '{{ image.guid|resize(' . $image->width . ', ' . round( $image->width * $args['ratio'] ) . ')|towebp(70) }} ' . $image->width . 'w' );
+		if ( ( $width - $args['steps'] ) < $image->width && ( $width - $args['steps'] ) < $args['max'] ) {
+			$height = $args['ratio'] ? round( $image->width * $args['ratio'] ) : 0;
+			array_push( $data['sizes_source'], '{{ image.guid|resize(' . $image->width . ', ' . $height . ') }} ' . $image->width . 'w' );
+			array_push( $data['sizes_webp'], '{{ image.guid|resize(' . $image->width . ', ' . $height . ')|towebp(70) }} ' . $image->width . 'w' );
 		}
 
 		return \Timber::compile_string(
 			'
-      {% if link is not empty %}
-        <a class="media-image__link" href="{{ link|e("esc_url") }}">
+    {% if link is not empty %}
+    <a class="media-image__link" href="{{ link|e("esc_url") }}">
+    {% endif %}
+    <picture>
+      <source data-srcset="' . implode( ', ', $data['sizes_webp'] ) . '" type="image/webp">
+      <source data-srcset="' . implode( ', ', $data['sizes_source'] ) . '" type="image/jpeg">
+      <img
+      class="{% if class is not empty %}{{ class }} {% endif %}lazyload js-lazyload"
+      {% if style is not empty %}
+      style="{{ style }}"
       {% endif %}
-        <picture>
-          {#<source data-srcset="' . implode( ', ', $data['sizes_webp'] ) . '" type="image/webp">#}
-          <source data-srcset="' . implode( ', ', $data['sizes_source'] ) . '" type="image/jpeg">
-          <img
-            class="{% if class is not empty %}{{ class }} {% endif %}lazyload js-lazyload"
-            {% if style is not empty %}
-            style="{{ style }}"
-            {% endif %}
-            alt="{{ image.alt }}"
-            src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-            data-src="{{ image.guid|resize(max.w, max.h) }}"
-            data-sizes="auto"
-            {{ fit }}>
-        </picture>
-      {% if link is not empty %}
-        </a>
+      alt="{{ image.alt }}"
+      src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+      data-src="{{ image.guid|resize(max.w, max.h) }}"
+      data-sizes="auto"
+      {{ fit }}>
+    </picture>
+    {% if link is not empty %}
+    </a>
+    {% endif %}
+    {% if copyright %}
+    <footer class="media-image__footer">
+      <small class="copyright">
+      {% if copyright_link %}
+        <a class="copyright__link" href="{{ copyright_link|e("esc_url") }}">{{ copyright }}</a>
+      {% else %}
+        {{ copyright }}
       {% endif %}
-      {% if copyright %}
-        <footer class="media-image__footer">
-          <small class="copyright">
-            {% if copyright_link %}
-              <a class="copyright__link" href="{{ copyright_link|e("esc_url") }}">{{ copyright }}</a>
-            {% else %}
-              {{ copyright }}
-            {% endif %}
-          </small>
-        </footer>
-      {% endif %}
-      ',
+      </small>
+    </footer>
+    {% endif %}
+		',
 			$data
 		);
+
 	}
 
 	public function the_cover_image_sizes( $image, $args = [] ) {
