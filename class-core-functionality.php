ffe8aa59c63856e537af8ef890e0af4a4ec1a48d
herrschuessler <schreib@herrschuessler.de>
2019-08-29 12:04:08 +0200

    Bump version

--- class-core-functionality.php
+++ class-core-functionality.php
@@ -9,7 +9,7 @@
  * @wordpress-plugin
  * Plugin Name: MONTAGMORGENS Core Functionality
  * Description: Dieses Plugin stellt die benötigten Funktionen für alle MONTAGMORGENS-WordPress-Themes zur Verfügung.
- * Version:     1.3.9
+ * Version:     1.4.0
  * Author:      MONTAGMORGENS GmbH
  * Author URI:  https://www.montagmorgens.com/
  * License:     GNU General Public License v.2
@@ -46,7 +46,7 @@ final class Core_Functionality {
 
 	use Helpers;
 
-	const PLUGIN_VERSION = '1.3.9';
+	const PLUGIN_VERSION = '1.4.0';
 	protected static $instance = null;
 
 	/**
--- readme.txt
+++ readme.txt
@@ -3,11 +3,14 @@ Contributors: herrschuessler
 Requires at least: 5.0.0
 Tested up to: 5.2
 Requires PHP: 7.0.0
-Stable tag: 1.3.9
+Stable tag: 1.4.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 == Changelog ==
+
+= 1.4.0 =
+* Add twig filter tel_link
 
 = 1.3.0 =
 * Add twig function get_all_posts
