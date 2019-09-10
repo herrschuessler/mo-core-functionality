dbe94052b3bd0ce0f0f8fc9140b972b0729e0144
herrschuessler <schreib@herrschuessler.de>
2019-09-10 08:27:17 +0200

    Update dependencies

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,28 +1,28 @@
 'use strict';
 
-const gulp        = require( 'gulp' );
-const requireDir  = require( 'require-dir' );
+const gulp = require( 'gulp' );
+const requireDir = require( 'require-dir' );
 
 // Require all tasks in gulpfile.js/tasks, including subfolders
 requireDir( './tasks', { recurse: true } )
 
 // Asset Task runs all assets but JS
 gulp.task(
-	'assets',
-	gulp.parallel( 'js' )
+  'assets',
+  gulp.parallel( 'js' )
 );
 
 // Build Task
 gulp.task(
-	'build',
-	gulp.series(
-		'clean',
-		gulp.parallel( 'assets' )
-	)
+  'build',
+  gulp.series(
+    'clean',
+    'assets'
+  )
 );
 
 // Default Task =  Dev Task
 gulp.task(
-	'default',
-	gulp.series( 'clean', 'assets', 'watch' )
+  'default',
+  gulp.series( 'clean', 'assets', 'watch' )
 );
