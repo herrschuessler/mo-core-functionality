189a08f56ae21572fe97f6b31a50272a93ba3750
herrschuessler <schreib@herrschuessler.de>
2019-12-03 12:28:55 +0100

    Add functions get_css_asset() and get_js_asset()

--- lib/functions/attach_style.php
+++ lib/functions/attach_style.php
@@ -10,13 +10,13 @@ function attach_style( $module ) {
 		return false;
 	}
 
-	// use minified scripts if not in dev mode
+	// use minified style if not in dev mode
 	$folder = ( defined( 'WP_ENV' ) && WP_ENV === 'development' ) ? 'dev/' : 'dist/';
 
 	// Script version = theme version
 	$version = wp_get_theme()['Version'];
 
-	$path = get_template_directory_uri() . '/assets/css/' . $folder . filter_var( $module, FILTER_SANITIZE_ENCODED ) . '.css?version=' . $version;
+	$path = get_template_directory_uri() . '/assets/css/' . $folder . filter_var( $module, FILTER_SANITIZE_ENCODED ) . '.css?ver=' . $version;
 
 	// @see https://jakearchibald.com/2016/link-in-body/
 	return sprintf( '<link rel="stylesheet" href="%s"><script> </script>', $path );
