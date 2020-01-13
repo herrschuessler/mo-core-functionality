db0bdc7037b61206360d7eb55da1a663a3920c22
herrschuessler <schreib@herrschuessler.de>
2020-01-13 16:17:00 +0100

    Add functionality note

--- lib/actions/grant-editor-privacy-cap.php
+++ lib/actions/grant-editor-privacy-cap.php
@@ -2,11 +2,14 @@
 /**
  * Grant editors access to privacy policy
  *
+ * This doesn't take effect until https://core.trac.wordpress.org/ticket/44176 is hopefully resolved.
+ *
  * @category   Plugin
  * @package    Mo\Core
  * @author     Christoph Schüßler <schuessler@montagmorgens.com>
  * @license    https://www.gnu.org/licenses/gpl-2.0.txt GNU/GPLv2
  * @since      1.13.0
+ * @see        https://core.trac.wordpress.org/ticket/44176
  */
 
 namespace Mo\Core\Action;
