5c7cf6984fa70f633cfb9264019a0da73505ee7d
herrschuessler <schreib@herrschuessler.de>
2020-03-02 14:57:03 +0100

    Add mo_social_links filter

--- lib/hooked_functions/twig-social-links.php
+++ lib/hooked_functions/twig-social-links.php
@@ -14,40 +14,37 @@
 namespace Mo\Core\Twig;
 
 /**
- * Setup global Timber context.
+ * Adds social links from Yoast SEO plugin to timber context.
  *
  * @param array $data The timber context variables.
  */
 function social_links( $data ) {
 	$seo_data = get_option( 'wpseo_social' );
 
-	if ( ! is_array( $seo_data ) ) {
-		return;
-	}
-
 	$social_links = [];
 
-	foreach ( $seo_data as $profile => $value ) {
-
-		if ( ! empty( $value ) ) {
-			switch ( $profile ) {
-				case 'facebook_site':
-				case 'instagram_url':
-				case 'linkedin_url':
-				case 'myspace_url':
-				case 'pinterest_url':
-				case 'youtube_url':
-				case 'wikipedia_url':
-					$social_links[ explode( '_', $profile )[0] ] = $value;
-					break;
-				case 'twitter_site':
-					$social_links[ explode( '_', $profile )[0] ] = 'https://twitter.com/' . $value . '/';
-					break;
+	if ( ! is_array( $seo_data ) ) {
+		foreach ( $seo_data as $profile => $value ) {
+			if ( ! empty( $value ) ) {
+				switch ( $profile ) {
+					case 'facebook_site':
+					case 'instagram_url':
+					case 'linkedin_url':
+					case 'myspace_url':
+					case 'pinterest_url':
+					case 'youtube_url':
+					case 'wikipedia_url':
+						$social_links[ explode( '_', $profile )[0] ] = $value;
+						break;
+					case 'twitter_site':
+						$social_links[ explode( '_', $profile )[0] ] = 'https://twitter.com/' . $value . '/';
+						break;
+				}
 			}
 		}
 	}
 
-	$data['social_links'] = $social_links;
+	$data['social_links'] = apply_filters( 'mo_social_links', $social_links );
 	return $data;
 }
 
