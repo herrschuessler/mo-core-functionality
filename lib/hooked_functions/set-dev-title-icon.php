71a0c481ea0b61f3c7e3158786b70c4c2571babb
herrschuessler <schreib@herrschuessler.de>
2020-03-16 11:10:29 +0100

    Add "hammer & wrench" icon before admin_title on DEV installs

--- lib/hooked_functions/set-dev-title-icon.php
+++ lib/hooked_functions/set-dev-title-icon.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Add icon to wp_title in DEV MODE
+ * Add icon to wp_title and admin_title in DEV MODE
  *
  * @category   Plugin
  * @package    Mo\Core
  * @author     Christoph Schüßler <schuessler@montagmorgens.com>
  * @license    https://www.gnu.org/licenses/gpl-2.0.txt GNU/GPLv2
- * @since      1.8.0
+ * @since      1.18.0
  */
 
 namespace Mo\Core\Action;
@@ -14,7 +14,7 @@ namespace Mo\Core\Action;
 use \Mo\Core\Core_Functionality as Core;
 
 /**
- * Add "hammer & wrench" icon before wp_title on DEV installs.
+ * Add "hammer & wrench" icon before title on DEV installs.
  *
  * @param string $title Page title.
  * @return string The title.
@@ -27,4 +27,5 @@ function set_dev_title_icon( $title ) {
 }
 
 \add_filter( 'wp_title', '\Mo\Core\Action\set_dev_title_icon', 999, 1 );
+\add_filter( 'admin_title', '\Mo\Core\Action\set_dev_title_icon', 999, 1 );
 
--- readme.txt
+++ readme.txt
@@ -3,11 +3,14 @@ Contributors: herrschuessler
 Requires at least: 5.0.0
 Tested up to: 5.3.2
 Requires PHP: 7.0.0
-Stable tag: 1.18.0
+Stable tag: 1.18.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 == Changelog ==
+
+= 1.18.1 =
+* Add "hammer & wrench" icon before admin_title on DEV installs
 
 = 1.18.0 =
 * Add "hammer & wrench" icon before wp_title on DEV installs
